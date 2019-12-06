b4c930c35c36616a29ca9e9e229fca89d7576bfb
Dmitry Arkhipov <grisumbras@gmail.com>
2019-12-06 16:05:22 +0300

    implement grid

--- test/conanfile.py
+++ test/conanfile.py
@@ -8,5 +8,7 @@ b2 = python_requires("b2-helper/[>=0.7.1]@grisumbras/stable")
 
 @b2.build_with_b2
 class MSassTestConan(ConanFile):
+    build_requires = "sassc/3.6.1@grisumbras/stable"
+
     def test(self):
         pass
